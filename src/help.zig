24ffca8f3a8197b24984aab750b094609bcfe945
nevvixsz <Ziglanguage@proton.me>
2026-08-01 21:33:17 +0700

    Create help.zig

---
+++ src/help.zig
@@ -0,0 +1,28 @@
+const std = @import("std");
+
+pub fn help() void {
+    std.debug.print(
+        \\zp - A minimal, source-based package manager
+        \\
+        \\Usage: zp [OPTIONS] [PACKAGE...]
+        \\
+        \\Options:
+        \\  --init        Initialize /var/zp and generate gen.sh
+        \\  -u            Update recipe trees and regenerate the database
+        \\  -s <pkg>      Search for a package in the database
+        \\  -i <pkg>      Download, build, and install a package from source
+        \\  -r <pkg>      Remove an installed package
+        \\  -h, --help    Show this help message and exit
+        \\  -v, --version Show version information and exit
+        \\
+        \\Examples:
+        \\  sudo zp --init              # Initialize the package manager
+        \\  sudo zp -u                  # Sync recipe trees (Void + Crux + KISS)
+        \\  sudo zp -s htop             # Search for htop
+        \\  sudo zp -i htop             # Build & install htop from source
+        \\  sudo zp -r htop             # Remove htop
+        \\
+        \\For more information, visit: https://github.com/nevvixsz/zp
+    , .{});
+    std.debug.print("\n", .{});
+}
