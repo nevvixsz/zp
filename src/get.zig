cb08f2584134c4f3ef3de5580f358519c708910e
nevvixsz <Ziglanguage@proton.me>
2026-08-20 17:10:27 +0700

    Update get.zig

--- src/get.zig
+++ src/get.zig
@@ -1,6 +1,12 @@
 const std = @import("std");
 
-pub fn getUrl(io: anytype, pkg: []const u8, buffer: []u8) ![]const u8 {
+pub const Package_Stat = struct {
+    name: []const u8,
+    version: []const u8,
+    url: []const u8,
+};
+
+pub fn GetPkgStatToInstall(io: anytype, pkg: []const u8, buffer: []u8) !Package_Stat {
     const file = try std.Io.Dir.cwd().openFile(io, "/var/zp/mirrors/zp.packages", .{});
     defer file.close(io);
 
@@ -9,9 +15,9 @@ pub fn getUrl(io: anytype, pkg: []const u8, buffer: []u8) ![]const u8 {
         var tokens = std.mem.tokenizeScalar(u8, line, ' ');
         const name = tokens.next() orelse continue;
         if (!std.mem.eql(u8, name, pkg)) continue;
-
+        const version = tokens.next() orelse return error.MalformedLine;
         const url = tokens.next() orelse return error.MalformedLine;
-        return url;
+        return .{ .name = name, .version = version, .url = url };
     }
     return error.PackageNotFound;
 }
@@ -25,9 +31,23 @@ pub fn getName(io: anytype, pkg: []const u8, buffer: []u8) !bool {
         var tokens = std.mem.tokenizeScalar(u8, line, ' ');
 
         const name = tokens.next() orelse continue;
-        if (std.mem.startsWith(u8, pkg, name)) {
+        if (std.mem.eql(u8, pkg, name)) {
             return true;
         }
     }
     return false;
+}
+
+pub fn getInstalledVersion(io: anytype, pkg: []const u8, buffer: []u8) !?[]const u8 {
+    const file = std.Io.Dir.cwd().openFile(io, "/var/zp/install/packages.db", .{}) catch return null;
+    defer file.close(io);
+
+    var reader = file.reader(io, buffer);
+    while (try reader.interface.takeDelimiter('\n')) |line| {
+        var tokens = std.mem.tokenizeScalar(u8, line, ' ');
+        const name = tokens.next() orelse continue;
+        if (!std.mem.eql(u8, name, pkg)) continue;
+        return tokens.next();
+    }
+    return null;
 }
