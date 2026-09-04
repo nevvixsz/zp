c897c765e78d854346416f9744801351ea3c0d38
lyrix <notdawk@proton.me>
2026-09-04 17:47:48 +0500

    nix: add dev shell

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Development shell for zp";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = function:
+        nixpkgs.lib.genAttrs systems (system: function nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = [ pkgs.zig_0_16 ];
+        };
+      });
+    };
+}
