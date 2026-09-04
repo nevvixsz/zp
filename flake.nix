73fde93eb7a708e4013ad88d456fc989980d87d0
lyrix <notdawk@proton.me>
2026-09-04 18:01:56 +0500

    nix: flake.lock + add zp pkg to flake.nix

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { nixpkgs, ... }:
+  outputs = { self, nixpkgs, ... }:
     let
       systems = [
         "x86_64-linux"
@@ -12,9 +12,40 @@
         "aarch64-darwin"
       ];
       forAllSystems = function:
-        nixpkgs.lib.genAttrs systems (system: function nixpkgs.legacyPackages.${system});
+        nixpkgs.lib.genAttrs systems (system: function (import nixpkgs {
+          inherit system;
+          config.allowUnsupportedSystem = true;
+        }));
+      packageFor = pkgs: pkgs.stdenv.mkDerivation {
+        pname = "zp";
+        version = "0.3.1";
+        src = self;
+        nativeBuildInputs = [ pkgs.zig_0_16 ];
+        buildPhase = "zig build -Doptimize=ReleaseSafe";
+        installPhase = ''
+          mkdir -p $out/bin
+          cp zig-out/bin/zp $out/bin/zp
+        '';
+        meta.mainProgram = "zp";
+      };
     in
     {
+      packages = forAllSystems (pkgs: {
+        default = packageFor pkgs;
+        zp = packageFor pkgs;
+      });
+
+      apps = forAllSystems (pkgs: {
+        default = {
+          type = "app";
+          program = "${packageFor pkgs}/bin/zp";
+        };
+        zp = {
+          type = "app";
+          program = "${packageFor pkgs}/bin/zp";
+        };
+      });
+
       devShells = forAllSystems (pkgs: {
         default = pkgs.mkShell {
           packages = [ pkgs.zig_0_16 ];
